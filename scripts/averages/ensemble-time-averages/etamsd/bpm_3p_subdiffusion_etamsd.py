Fixed the time average scaling estimations; now working properly. Added various types of averages and some refactors.
Needs more refactoring!

scripts/averages/ensemble-time-averages/etamsd/bpm_3p_subdiffusion_etamsd.py
@@ -8,28 +8,25 @@ from domain.processes.bpm_3p_process import BPM3pProcess
 bpm3p = BPM3pProcess(0.25, 1, 1)
 
 averager = EnsembleTimeAverager()
-T = 10000
+T = 1000
 N = 100
-step_length = 1
-delta_max = 1000
+time_step = 1
+delta_max = 100
 delta_axis = np.arange(1, delta_max + 1)
 etamsd_delta = []
+
 for delta in delta_axis:
-    etamsd = averager.etamsd(bpm3p, N, T, delta, step_length)
+    etamsd = averager.etamsd(bpm3p, N, T, delta, time_step)
     etamsd_delta.append(etamsd)
 
 log_deltas = np.log(delta_axis)
 log_etamsd = np.log(etamsd_delta)
 
-
 slope, intercept, r_value, p_value, std_err = linregress(log_deltas, log_etamsd)
-alpha = slope
-print(f"Estimated scaling exponent α: {alpha}")
 
 fig, ax = plt.subplots(figsize=(8, 5))
-# ax.plot(delta_axis, tamsd_delta, label='TAMSD')
 
-plt.loglog(delta_axis, etamsd_delta, 'o-', label='ETAMSD')
+plt.loglog(delta_axis, etamsd_delta, 'o-', label='ETAMSD - Slope = ' + str(slope))
 plt.xlabel('Time lag (Δ)')
 plt.ylabel('ETAMSD')
 plt.title('Log-Log Plot of ETAMSD')

scripts/averages/time-averages/tamsd/bpm-3p_subdiffusion_tamsd.py
@@ -5,7 +5,7 @@ from scipy.stats import linregress
 from domain.averagers.time_averager import TimeAverager
 from domain.processes.bpm_3p_process import BPM3pProcess
 
-bpm3p = BPM3pProcess(0.25, 1, 1)
+bpm3p = BPM3pProcess(1/4, 1, 1)
 
 averager = TimeAverager()
 T = 10000
@@ -15,19 +15,18 @@ delta_axis = np.arange(1, delta_max + 1)
 tamsd_delta = []
 for delta in delta_axis:
     tamsd = averager.tamsd(bpm3p, T, delta, step_length)
+    if tamsd == 0:
+        tamsd = np.min(tamsd_delta)
     tamsd_delta.append(tamsd)
 
 log_deltas = np.log(delta_axis)
 log_tamsd = np.log(tamsd_delta)
 
 slope, intercept, r_value, p_value, std_err = linregress(log_deltas, log_tamsd)
-alpha = slope
-print(f"Estimated scaling exponent α: {alpha}")
 
 fig, ax = plt.subplots(figsize=(8, 5))
-# ax.plot(delta_axis, tamsd_delta, label='TAMSD')
 
-plt.loglog(delta_axis, tamsd_delta, 'o-', label='TAMSD')
+plt.loglog(delta_axis, tamsd_delta, 'o-', label='TAMSD- Slope = ' + str(slope))
 plt.xlabel('Time lag (Δ)')
 plt.ylabel('TAMSD')
 plt.title('Log-Log Plot of TAMSD')

src/domain/averagers/ensemble_averager.py
@@ -7,15 +7,15 @@ class EnsembleAverager:
 
     # N is the total of simulations to run
     # T is the total time units to simulate
-    # step_length is the timestep between sucessive observations, in time units. Default is 1
-    def average(self, model, N, T, step_length=1):
-        ensemble = self.generate_ensemble(model, N, T, step_length)
+    # Time step is the timestep between sucessive observations, in time units. Default is 1
+    def average(self, model, N, T, time_step=1):
+        ensemble = self.generate_ensemble(model, N, T, time_step)
         average = np.mean(ensemble, axis=0)
         return average.tolist()
 
-    def generate_ensemble(self, model, N, T, step_length):
+    def generate_ensemble(self, model, N, T, time_step):
         ensemble = []
         for i in range(N):
-            sample_path = create_normalized_sample_path(model, T, step_length)
+            sample_path = create_normalized_sample_path(model, T, time_step)
             ensemble.append(sample_path)
         return ensemble

src/domain/averagers/ensemble_time_averager.py
@@ -5,20 +5,32 @@ from domain.averagers.time_averager import TimeAverager
 
 class EnsembleTimeAverager:
 
+    def ensemble_time_absolute_velocity_average(self, model, N, T, window_length, time_step):
+        ensemble = self.generate_absolute_velocity_ensemble(model, N, T, window_length, time_step)
+        return np.mean(ensemble, axis=0)
+
     # Time: total time to simulate
-    # Step length: timestep between computed observations
+    # Time step: timestep between computed observations
     # N: Number of repetitions to average
     # Delta: Length of displacement to compute
-    def etamsd(self, model, N, T, delta, step_length):
-        ensemble = self.generate_ensemble(model, N, T, delta, step_length)
-        average = np.mean(ensemble)
-        return average
+    def etamsd(self, model, N, T, delta, time_step):
+        ensemble = self.generate_tamsd_ensemble(model, N, T, delta, time_step)
+        return np.mean(ensemble)
 
-    def generate_ensemble(self, model, N, T, delta, step_length):
+    def generate_absolute_velocity_ensemble(self, model, N, T, window_length, time_step):
         time_averager = TimeAverager()
         ensemble = []
         for i in range(N):
-            tamsd = time_averager.tamsd(model, T, delta, step_length)
+            absolute_average = time_averager.absolute_velocity_average(model, T, window_length, time_step)
+            ensemble.append(absolute_average)
+        return ensemble
+
+    def generate_tamsd_ensemble(self, model, N, T, delta, time_step):
+        time_averager = TimeAverager()
+        ensemble = []
+        for i in range(N):
+            tamsd = time_averager.tamsd(model, T, delta, time_step)
             ensemble.append(tamsd)
         return ensemble
-    
+
+

src/domain/averagers/time_averager.py
@@ -1,3 +1,5 @@
+import warnings
+
 import numpy as np
 
 from domain.extra_functions import extra_functions
@@ -5,23 +7,41 @@ from domain.extra_functions import extra_functions
 
 class TimeAverager:
 
-    # Window length is measured in samples, i.e., multiples of step_length
-    def average(self, model, T, window_length, step_length=1):
-        states = extra_functions.create_normalized_sample_path(model, T, step_length)
-        chunked_array = np.array(extra_functions.chunk_array(states, window_length))
-        return np.mean(chunked_array, axis=0).tolist()
+    # Window length is measured in samples, i.e., multiples of time_step
+    def average(self, model, T, delta, time_step=1):
+        displacements = self.generate_displacements_array(model, T, delta, time_step)
+        return np.mean(displacements)
 
-    # delta should be a multiple of step length. Step length is recommended to be 1 to avoid issues
-    def tamsd(self, model, T, delta, step_length=1):
-        delta = int(delta)  # Just to avoid numpy type issues
-        X = extra_functions.create_normalized_sample_path(model, T, step_length)
-        N = T * step_length
-        m = int(delta/step_length)
+    def absolute_average(self, model, T, delta, time_step=1):
+        displacements = self.generate_displacements_array(model, T, delta, time_step, average_type='abs')
+        return np.mean(np.abs(displacements))
+
+    def absolute_velocity_average(self, model, T, delta, time_step=1):
+        displacements = self.generate_displacements_array(model, T, delta, time_step, average_type='abs_vel')
+        return np.mean(np.abs(displacements))
+
+    # delta should be a multiple of time step.
+    def tamsd(self, model, T, delta, time_step=1):
+        displacements = self.generate_displacements_array(model, T, delta, time_step, average_type='msd')
+        return np.mean(displacements)
+
+    def generate_displacements_array(self, model, T, delta, time_step, average_type=None):
+        delta = int(delta)  # Ensure it is an integer
+        X = extra_functions.create_normalized_sample_path(model, T, time_step)
+        N = int(T * time_step)
+        m = int(delta / time_step)
         displacements = []
-
+        if m >= N:
+            m -= 1
+            warnings.warn("Delta is too large for the given T and step_length. Truncating the last observation.")
         for k in range(0, N - m):
-            displacement = (X[k + delta] - X[k]) ** 2
+            if average_type == 'msd':
+                displacement = (X[k + delta] - X[k]) ** 2
+            elif average_type == 'abs':
+                displacement = np.abs(X[k + delta] - X[k])
+            elif average_type == 'abs_vel':
+                displacement = np.abs((X[k + delta] - X[k])/delta)
+            else:
+                displacement = X[k + delta] - X[k]
             displacements.append(displacement)
-
-        tamsd = np.mean(displacements)
-        return tamsd
+        return displacements