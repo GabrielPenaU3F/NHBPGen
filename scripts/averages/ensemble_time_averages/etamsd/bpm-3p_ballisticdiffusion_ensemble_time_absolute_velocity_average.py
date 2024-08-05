Several fixes in the estimation of the scaling exponents. Now everything works fine.

scripts/averages/ensemble_time_averages/etamsd/bpm-3p_ballisticdiffusion_ensemble_time_absolute_velocity_average.py
@@ -5,29 +5,35 @@ from scipy.stats import linregress
 from domain.averagers.ensemble_time_averager import EnsembleTimeAverager
 from domain.processes.bpm_3p_process import BPM3pProcess
 
-bpm3p = BPM3pProcess(0.25, 1, 1)
+bpm3p = BPM3pProcess(1, 1, 1)
 
 averager = EnsembleTimeAverager()
+window_length = 100
 N = 100
-max_T = 1000
+T = 1000
 time_step = 1
-delta = 10
+delta_max = 100
+delta_axis = np.arange(1, delta_max + 1)
+absavg_delta = []
 
-vel_avgs = []
-t = np.arange(100, max_T, 1)
-for T in t:
-    vel_avg = averager.average(bpm3p, N, T, time_step, average_type='abs-vel')
-    vel_avgs.append(vel_avg)
+for delta in delta_axis:
+    absavg = averager.ensemble_time_absolute_velocity_average(bpm3p, N, T, delta, time_step)
+    if absavg == 0:
+        absavg = np.min(absavg_delta)
+    absavg_delta.append(absavg)
 
-slope, intercept, r_value, p_value, std_err = linregress(np.log(t), np.log(vel_avgs))
+log_deltas = np.log(delta_axis)
+log_vel_avg = np.log(absavg_delta)
+
+slope, intercept, r_value, p_value, std_err = linregress(log_deltas, log_vel_avg)
 
 fig, ax = plt.subplots(figsize=(8, 5))
 
-plt.loglog(t, vel_avgs, 'o-', label='Absolute velocity average - Slope = ' + str(slope))
+plt.loglog(log_deltas, log_vel_avg, 'o-', label='Absolute velocity average - Slope = ' + str(slope))
 
 
-ax.set_title('Absolute velocity ET-average (subdiffusion)', fontsize=14)
-ax.set_xlabel('Time (t)', fontsize=11)
+ax.set_title('Absolute velocity ET-average (Normal diffusion)', fontsize=14)
+ax.set_xlabel('Time lag (Δ)', fontsize=11)
 ax.xaxis.set_tick_params(labelsize=10)
 ax.xaxis.labelpad = 4
 ax.set_ylabel('Absolute velocity', fontsize=11)

scripts/averages/time_averages/tamsd/bpm-3p_subdiffusion_tamsd.py
@@ -10,7 +10,7 @@ bpm3p = BPM3pProcess(1/4, 1, 1)
 averager = TimeAverager()
 T = 10000
 step_length = 1
-delta_max = 1000
+delta_max = 100
 delta_axis = np.arange(1, delta_max + 1)
 tamsd_delta = []
 for delta in delta_axis:

src/domain/averagers/displacement_averaging_strategies/displacement_averaging_strategies.py
@@ -6,28 +6,27 @@ import numpy as np
 
 class DisplacementAveragingStrategy(ABC):
     @abstractmethod
-    def calculate(self, X: List[float], k: int, delta: int) -> float:
+    def calculate(self, X: List[float], T: float, delta: int) -> float:
         pass
 
 
 class RegularAveragingStrategy(DisplacementAveragingStrategy):
 
-    def calculate(self, X, k, delta):
-        return X[k + delta] - X[k]
+    def calculate(self, X, T, delta):
+        pass
 
 
 class AbsoluteAveragingStrategy(DisplacementAveragingStrategy):
 
-    def calculate(self, X, k, delta):
-        return np.abs(X[k + delta] - X[k])
+    def calculate(self, X, T, delta):
+        pass
 
 
 class AbsoluteVelocityAveragingStrategy(DisplacementAveragingStrategy):
 
-    def calculate(self, X, k, delta):
-        return np.abs((X[k + delta] - X[k])/delta)
+    def calculate(self, X, T, delta):
+        sum = 0
+        for j in range(1, int(T/delta)):
+            sum += X[j * delta] - X[(j-1) * delta]
 
-
-class SquaredAveragingStrategy(DisplacementAveragingStrategy):
-    def calculate(self, X, k, delta):
-        return (X[k + delta] - X[k])**2
+        return sum/(T - delta)

src/domain/averagers/ensemble_time_averager.py
@@ -5,9 +5,9 @@ from domain.averagers.time_averager import TimeAverager
 
 class EnsembleTimeAverager:
 
-    def ensemble_time_absolute_velocity_average(self, model, N, T, window_length, time_step):
-        ensemble = self.generate_absolute_velocity_ensemble(model, N, T, window_length, time_step)
-        return np.mean(ensemble, axis=0)
+    def average(self, model, N, T, time_step, average_type='regular'):
+        ensemble = self.generate_ensemble(model, N, T, time_step, average_type)
+        return np.mean(ensemble)
 
     # Time: total time to simulate
     # Time step: timestep between computed observations
@@ -17,12 +17,12 @@ class EnsembleTimeAverager:
         ensemble = self.generate_tamsd_ensemble(model, N, T, delta, time_step)
         return np.mean(ensemble)
 
-    def generate_absolute_velocity_ensemble(self, model, N, T, window_length, time_step):
+    def generate_ensemble(self, model, N, T, time_step, average_type):
         time_averager = TimeAverager()
         ensemble = []
         for i in range(N):
-            absolute_average = time_averager.absolute_velocity_average(model, T, window_length, time_step)
-            ensemble.append(absolute_average)
+            average = time_averager.average(model, T, time_step, average_type)
+            ensemble.append(average)
         return ensemble
 
     def generate_tamsd_ensemble(self, model, N, T, delta, time_step):

src/domain/averagers/time_averager.py
@@ -3,7 +3,7 @@ import warnings
 import numpy as np
 
 from domain.averagers.displacement_averaging_strategies.displacement_averaging_strategies import \
-    RegularAveragingStrategy, AbsoluteAveragingStrategy, AbsoluteVelocityAveragingStrategy, SquaredAveragingStrategy
+    RegularAveragingStrategy, AbsoluteAveragingStrategy, AbsoluteVelocityAveragingStrategy
 from domain.extra_functions import extra_functions
 
 
@@ -16,28 +16,24 @@ class TimeAverager:
             'regular': RegularAveragingStrategy,
             'abs': AbsoluteAveragingStrategy,
             'abs-vel': AbsoluteVelocityAveragingStrategy,
-            'msd': SquaredAveragingStrategy
         }
 
     # Window length is measured in samples, i.e., multiples of time_step
-    def average(self, model, T, delta, time_step=1):
-        displacements = self.generate_displacements_array(model, T, delta, time_step)
-        return np.mean(displacements)
+    def average(self, model, T, time_step=1, average_type='regular'):
+        states = extra_functions.create_normalized_sample_path(model, T, time_step)
 
-    def absolute_average(self, model, T, delta, time_step=1):
-        displacements = self.generate_displacements_array(model, T, delta, time_step, average_type='abs')
-        return np.mean(np.abs(displacements))
+        strategy_class = self.strategies.get(average_type)
+        if not strategy_class:
+            raise ValueError(f"Unknown average type: {average_type}")
 
-    def absolute_velocity_average(self, model, T, delta, time_step=1):
-        displacements = self.generate_displacements_array(model, T, delta, time_step, average_type='abs-vel')
-        return np.mean(np.abs(displacements))
+        return strategy_class().calculate(states, T, time_step)
 
     # delta should be a multiple of time step.
     def tamsd(self, model, T, delta, time_step=1):
-        displacements = self.generate_displacements_array(model, T, delta, time_step, average_type='msd')
+        displacements = self.generate_displacements_array(model, T, delta, time_step)
         return np.mean(displacements)
 
-    def generate_displacements_array(self, model, T, delta, time_step, average_type='regular'):
+    def generate_displacements_array(self, model, T, delta, time_step):
         delta = int(delta)  # Ensure it is an integer
         X = extra_functions.create_normalized_sample_path(model, T, time_step)
         N = int(T * time_step)
@@ -47,12 +43,7 @@ class TimeAverager:
             m -= 1
             warnings.warn("Delta is too large for the given T and step_length. Truncating the last observation.")
 
-        strategy_class = self.strategies.get(average_type)
-        if not strategy_class:
-            raise ValueError(f"Unknown average type: {average_type}")
-
-        averaging_strategy = strategy_class()
         for k in range(0, N - m):
-            displacement = averaging_strategy.calculate(X, k, delta)
+            displacement = (X[k + delta] - X[k]) ** 2
             displacements.append(displacement)
         return displacements