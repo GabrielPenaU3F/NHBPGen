Fixed the last bug found in the Ensemble time averager and Time averager. Now it works as it should

scripts/averages/ensemble_time_averages/bpm-3p_ballistic_diffusion_ensemble_time_absolute_velocity_average.py
@@ -5,7 +5,7 @@ from scipy.stats import linregress
 from domain.averagers.ensemble_time_averager import EnsembleTimeAverager
 from domain.processes.bpm_3p_process import BPM3pProcess
 
-bpm3p = BPM3pProcess(0.25, 1, 1)
+bpm3p = BPM3pProcess(1, 1, 1)
 
 averager = EnsembleTimeAverager()
 N = 100
@@ -22,7 +22,7 @@ fig, ax = plt.subplots(figsize=(8, 5))
 plt.loglog(t, vel_avgs, 'o-', label='Absolute velocity average - Slope = ' + str(slope))
 
 
-ax.set_title('Absolute velocity ET-average (subdiffusion)', fontsize=14)
+ax.set_title('Absolute velocity ET-average (ballistic diffusion)', fontsize=14)
 ax.set_xlabel('Time (t)', fontsize=11)
 ax.xaxis.set_tick_params(labelsize=10)
 ax.xaxis.labelpad = 4

src/domain/averagers/ensemble_time_averager.py
@@ -1,13 +1,22 @@
 import numpy as np
 
 from domain.averagers.time_averager import TimeAverager
+from domain.sampler import Sampler
 
 
 class EnsembleTimeAverager:
 
+    # See e.g. Eq. B1 from Aghion et al. (2021) or Eq. 4 from Vilk et al. (2022)
     def average(self, model, N, T, time_step, average_type='regular'):
         ensemble = self.generate_ensemble(model, N, T, time_step, average_type)
         return np.mean(ensemble)
+
+    # See e.g. Eqs. 4 and 5 from Aghion et al. (2021)
+    # min_T: minimum time location over which to compute the average of displacements
+    # max_T: maximum of those
+    def average_as_function_of_t(self, model, N, min_T, max_T, time_step, average_type='regular'):
+        ensemble = self.generate_ensemble_as_function_of_t(model, N, min_T, max_T, time_step, average_type)
+        return np.mean(ensemble, axis=0)
 
     # Time: total time to simulate
     # Time step: timestep between computed observations
@@ -21,7 +30,8 @@ class EnsembleTimeAverager:
         time_averager = TimeAverager()
         ensemble = []
         for i in range(N):
-            average = time_averager.average(model, T, time_step, average_type)
+            observations = Sampler().generate_observations_sample_path(model, T, time_step, plot=False)
+            average = time_averager.average(observations, T, time_step, average_type)
             ensemble.append(average)
         return ensemble
 
@@ -33,4 +43,16 @@ class EnsembleTimeAverager:
             ensemble.append(tamsd)
         return ensemble
 
-
+    def generate_ensemble_as_function_of_t(self, model, N, min_T, max_T, time_step, average_type):
+        time_averager = TimeAverager()
+        ensemble = []
+        t_axis = np.arange(min_T, max_T, time_step)
+        for i in range(N):
+            avgs = []
+            observations_sample_path = Sampler().generate_observations_sample_path(model, max_T, time_step, plot=False)
+            for T in t_axis:
+                avg = time_averager.average(observations_sample_path, T, time_step, average_type)
+                avgs.append(avg)
+            ensemble.append(avgs)
+            print(f"Generating trajectory n={i+1} ...")
+        return np.array(ensemble)