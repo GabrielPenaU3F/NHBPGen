Fixed a bug in the averaging strategy

src/domain/averagers/displacement_averaging_strategies/displacement_averaging_strategies.py
@@ -25,8 +25,10 @@ class AbsoluteAveragingStrategy(DisplacementAveragingStrategy):
 class AbsoluteVelocityAveragingStrategy(DisplacementAveragingStrategy):
 
     def calculate(self, X, T, delta):
+        n = int(T/delta)
         sum = 0
-        for j in range(1, int(T/delta)):
-            sum += X[j * delta] - X[(j-1) * delta]
+        for j in range(1, n):
+            sum += np.abs(X[j * delta] - X[(j-1) * delta])/delta
 
-        return sum/(T - delta)
+        # return n * sum
+        return sum/T

src/domain/averagers/time_averager.py
@@ -15,12 +15,12 @@ class TimeAverager:
         'abs-vel': AbsoluteVelocityAveragingStrategy,
     }
 
-    def average(self, observations_sample_path, T, time_step=1, average_type='regular'):
+    def average(self, observations_sample_path, T, delta, average_type='regular'):
         strategy_class = self.strategies.get(average_type)
         if not strategy_class:
             raise ValueError(f"Unknown average type: {average_type}")
 
-        return strategy_class().calculate(observations_sample_path, T, time_step)
+        return strategy_class().calculate(observations_sample_path, T, delta)
 
     def tamsd(self, model, T, min_delta, max_delta, time_step=1):
         delta_axis = np.arange(min_delta, max_delta + 1)