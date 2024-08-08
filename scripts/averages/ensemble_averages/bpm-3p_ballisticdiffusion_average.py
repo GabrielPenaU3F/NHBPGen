Refactored ensemble averager into averaging strategies and fixed some old scripts from ensemble averaging

scripts/averages/ensemble_averages/bpm-3p_ballisticdiffusion_average.py
@@ -9,10 +9,10 @@ bpm3p = BPM3pProcess(1, 1, 1)
 averager = EnsembleAverager()
 N = 1000
 T = 1000
-step_length = 1
-average = averager.average(bpm3p, N, T, step_length)
+time_step = 1
+average = averager.average(bpm3p, N, T, time_step)
 
-t = np.linspace(0, T, int(T/step_length))
+t = np.linspace(0, T, int(T / time_step))
 mv = bpm3p.mean_value(t)
 
 fig, ax = plt.subplots(figsize=(8, 5))

scripts/averages/ensemble_averages/bpm-3p_hyperballistic_average.py
@@ -1,7 +1,7 @@
 import numpy as np
 from matplotlib import pyplot as plt
 
-from domain.calculators.ensemble_averager import EnsembleAverager
+from domain.averagers.ensemble_averager import EnsembleAverager
 from domain.processes.bpm_3p_process import BPM3pProcess
 
 bpm3p = BPM3pProcess(1.5, 1, 1)
@@ -9,10 +9,10 @@ bpm3p = BPM3pProcess(1.5, 1, 1)
 averager = EnsembleAverager()
 N = 1000
 T = 100
-step_length = 1
-average = averager.average(bpm3p, N, T, step_length)
+time_step = 1
+average = averager.average(bpm3p, N, T, time_step)
 
-t = np.linspace(0, T, int(T/step_length))
+t = np.linspace(0, T, int(T / time_step))
 mv = bpm3p.mean_value(t)
 
 fig, ax = plt.subplots(figsize=(8, 5))

src/domain/averagers/ensemble_averager.py
@@ -1,17 +1,27 @@
 import numpy as np
 
+from domain.averagers.averaging_strategies.ensemble_averaging_strategies import RegularEnsembleAveragingStrategy, \
+    SquareEnsembleAveragingStrategy
 from domain.sampler import Sampler
 
 
 class EnsembleAverager:
 
+    strategies = {
+        'regular': RegularEnsembleAveragingStrategy,
+        'sq': SquareEnsembleAveragingStrategy,
+    }
+
     # N is the total of simulations to run
     # T is the total time units to simulate
     # Time step is the timestep between sucessive observations, in time units. Default is 1
-    def average(self, model, N, T, time_step=1):
+    def average(self, model, N, T, time_step=1, average_type='regular'):
         ensemble = self.generate_ensemble(model, N, T, time_step)
-        average = np.mean(ensemble, axis=0)
-        return average.tolist()
+        strategy_class = self.strategies.get(average_type)
+        if not strategy_class:
+            raise ValueError(f"Unknown average type: {average_type}")
+
+        return strategy_class().calculate(ensemble)
 
     def generate_ensemble(self, model, N, T, time_step):
         ensemble = []