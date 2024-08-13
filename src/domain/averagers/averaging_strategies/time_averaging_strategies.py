Fixed a minor bug in the average calculations. Added exponent estimation scripts

src/domain/averagers/averaging_strategies/time_averaging_strategies.py
@@ -18,8 +18,7 @@ class AbsoluteVelocityTimeAveragingStrategy(TimeAveragingStrategy):
         for j in range(1, n):
             sum += np.abs(X[j * delta] - X[(j-1) * delta])/delta
 
-        # return n * sum
-        return sum/(T - delta)
+        return sum/n
 
 
 class SquareVelocityTimeAveragingStrategy(TimeAveragingStrategy):
@@ -30,5 +29,4 @@ class SquareVelocityTimeAveragingStrategy(TimeAveragingStrategy):
         for j in range(1, n):
             sum += ((X[j * delta] - X[(j-1) * delta])/delta) ** 2
 
-        # return n * sum
-        return sum/(T - delta)
+        return sum/n