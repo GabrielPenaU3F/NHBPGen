9e71653a22eb3ef86f3551157a22a75745553ff4
Gabriel <gpena@untref.edu.ar>
2024-08-13 03:12:08 -0300

    Added more scripts

--- scripts/averages/ensemble_time_averages/etamsd/bpm_3p_ballistic_diffusion_etamsd.py
+++ scripts/averages/ensemble_time_averages/etamsd/bpm_3p_ballistic_diffusion_etamsd.py
@@ -8,23 +8,21 @@ from domain.processes.bpm_3p_process import BPM3pProcess
 bpm3p = BPM3pProcess(1, 1, 1)
 
 averager = EnsembleTimeAverager()
-T = 500
+T = 1000
 N = 100
 time_step = 1
-delta_min = 1
-delta_max = 50
+delta_min = 10
+delta_max = 100
 delta_axis = np.arange(delta_min, delta_max + 1)
 
-etamsd_delta = averager.etamsd(bpm3p, N, T, delta_min, delta_max, time_step)
+etamsd = averager.etamsd(bpm3p, N, T, delta_min, delta_max, time_step)
 
-log_deltas = np.log(delta_axis)
-log_etamsd = np.log(etamsd_delta)
-
-slope, intercept, r_value, p_value, std_err = linregress(log_deltas, log_etamsd)
+slope, intercept, r_value, p_value, std_err = linregress(np.log(delta_axis), np.log(etamsd))
+J = slope / 2
 
 fig, ax = plt.subplots(figsize=(8, 5))
 
-ax.loglog(delta_axis, etamsd_delta, 'o-', label='ETAMSD - Slope = ' + str(slope))
+ax.loglog(delta_axis, etamsd, 'o-', label='ETAMSD', color='#3A1078')
 ax.set_xlabel('Time lag (Δ)', fontsize=11)
 ax.xaxis.set_tick_params(labelsize=10)
 ax.xaxis.labelpad = 4
@@ -38,7 +36,15 @@ ax.patch.set_facecolor("#ffffff")
 ax.patch.set_edgecolor('black')
 ax.patch.set_linewidth(1)
 ax.set_facecolor("#ffffff")
-ax.legend(fontsize=12)
+legend = ax.legend(fontsize=12, loc='upper left')
+
+# Annotation
+legend_bbox = legend.get_window_extent()
+legend_bbox = legend_bbox.transformed(fig.transFigure.inverted())
+y_pos = legend_bbox.y0  # Adjust this value to position the text as needed
+plt.annotate(f'Slope: {'%.3f'%(slope)}', xy=(0.11, y_pos), xycoords='figure fraction', fontsize=11)
+plt.annotate(f'J = {'%.3f'%(J)}', xy=(0.11, y_pos - 0.05), xycoords='figure fraction', fontsize=11)
+
 fig.tight_layout()
 
 plt.show()
