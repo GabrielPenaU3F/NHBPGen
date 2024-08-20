Mini refactor to the time-averager and ensemble-time-averager to correct a responsibility

scripts/averages/time_averages/bpm-3p_subdiffusion_time_absolute_average.py
@@ -10,22 +10,20 @@ bpm3p = BPM3pProcess(0.25, 1, 1)
 
 averager = TimeAverager()
 N = 100
+min_T = 100
 max_T = 1000
 time_step = 1
 delta = 10
 
-vel_avgs = []
-t_axis = np.arange(100, max_T, 1)
-states_sample_path = Sampler().simulate_sample_path(bpm3p, max_T, path_type='observations', time_step=time_step, plot=False)
-for t in t_axis:
-    vel_avg = averager.average(states_sample_path, t, time_step, average_type='abs')
-    vel_avgs.append(vel_avg)
+t_axis = np.arange(min_T, max_T, 1)
+sample_path = Sampler().simulate_sample_path(bpm3p, max_T, path_type='observations', time_step=time_step, plot=False)
+avg_t = averager.time_average_as_function_of_t(sample_path, min_T, max_T, time_step, average_type='abs')
 
-slope, intercept, r_value, p_value, std_err = linregress(np.log(t_axis), np.log(vel_avgs))
+slope, intercept, r_value, p_value, std_err = linregress(np.log(t_axis), np.log(avg_t))
 
 fig, ax = plt.subplots(figsize=(8, 5))
 
-plt.loglog(t_axis, vel_avgs, 'o-', label='Absolute velocity average - Slope = ' + str(slope))
+plt.loglog(t_axis, avg_t, 'o-', label='Absolute velocity average - Slope = ' + str(slope))
 
 
 ax.set_title('Absolute velocity time average (subdiffusion)', fontsize=14)

src/domain/averagers/ensemble_time_averager.py
@@ -34,15 +34,11 @@ class EnsembleTimeAverager:
 
     def time_average_ensemble_as_function_of_t(self, ensemble, min_T, max_T, time_step, average_type):
         time_averager = TimeAverager()
-        t_axis = np.arange(min_T, max_T, time_step)
         avg_ensemble_t = []
         for i in range(len(ensemble)):
             path = ensemble[i]
-            avgs = []
-            for T in t_axis:
-                avg = time_averager.average(path, T, time_step, average_type)
-                avgs.append(avg)
-            avg_ensemble_t.append(avgs)
+            avg_t = time_averager.time_average_as_function_of_t(path, min_T, max_T, time_step, average_type)
+            avg_ensemble_t.append(avg_t)
             print(f"Time-averaging trajectory n={i+1} ...")
         return np.array(avg_ensemble_t)
 