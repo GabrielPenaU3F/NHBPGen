Refactored Moses exponent calculation and fixed some scripts

scripts/averages/ensemble_time_averages/bpm-3p_ballistic_diffusion_ensemble_time_square_average.py
@@ -16,19 +16,20 @@ time_step = 1
 
 t = np.arange(min_T, max_T, time_step)
 ensemble = sampler.generate_ensemble(bpm3p, N, max_T, path_type='observations', time_step=time_step)
-vel_avgs = averager.average_as_function_of_t(ensemble, min_T, max_T, time_step, average_type='abs-vel')
-slope, intercept, r_value, p_value, std_err = linregress(np.log(t), np.log(vel_avgs))
-M = slope + 1/2
+avgs_t = averager.average_as_function_of_t(ensemble, min_T, max_T, time_step, average_type='sq')
+slope, intercept, r_value, p_value, std_err = linregress(np.log(t), np.log(avgs_t))
+M = 0.503
+L = (slope - 2 * M + 2) / 2
 
 fig, ax = plt.subplots(figsize=(8, 5))
-plt.loglog(t, vel_avgs, 'o-', label='Absolute velocity average', color='#3A1078')
+plt.loglog(t, avgs_t, 'o-', label='Square velocity average', color='#3A1078')
 
 
-ax.set_title('Absolute velocity ET-average (ballistic diffusion)', fontsize=14)
+ax.set_title('Square velocity ET-average (ballistic diffusion)', fontsize=14)
 ax.set_xlabel('Time (t)', fontsize=11)
 ax.xaxis.set_tick_params(labelsize=10)
 ax.xaxis.labelpad = 4
-ax.set_ylabel('Absolute velocity', fontsize=11)
+ax.set_ylabel('Square velocity', fontsize=11)
 ax.yaxis.set_tick_params(labelsize=10)
 ax.yaxis.labelpad = 8
 ax.set_xlim(auto=True)
@@ -40,12 +41,19 @@ ax.set_facecolor("#ffffff")
 legend = ax.legend(fontsize=12, loc='upper left')
 
 # Annotation
+# x_pos = 0.95
+# y_pos = 0.95  # Adjust this value to position the text as needed
+# plt.annotate(f'Slope: {'%.3f'%(slope)}', xy=(x_pos, y_pos), xycoords='axes fraction', fontsize=11,
+#              horizontalalignment='right', verticalalignment='top')
+# plt.annotate(f'L = {'%.3f'%(L)}, M = {'%.3f'%(M)}', xy=(x_pos, y_pos - 0.05), xycoords='axes fraction',
+#              fontsize=11, horizontalalignment='right', verticalalignment='top')
+
 legend_bbox = legend.get_window_extent()
 legend_bbox = legend_bbox.transformed(fig.transFigure.inverted())
-x_pos = legend_bbox.x0 + 0.045
+x_pos = legend_bbox.x0 + 0.04
 y_pos = legend_bbox.y0  # Adjust this value to position the text as needed
 plt.annotate(f'Slope: {'%.3f'%(slope)}', xy=(x_pos, y_pos), xycoords='figure fraction', fontsize=11)
-plt.annotate(f'M = {'%.3f'%(M)}', xy=(x_pos, y_pos - 0.05), xycoords='figure fraction', fontsize=11)
+plt.annotate(f'L = {'%.3f'%(L)}, M = {'%.3f'%(M)}', xy=(x_pos, y_pos - 0.05), xycoords='figure fraction', fontsize=11)
 
 fig.tight_layout()
 

scripts/averages/ensemble_time_averages/bpm-3p_subdiffusion_ensemble_time_absolute_average.py
@@ -6,26 +6,25 @@ from domain.averagers.ensemble_time_averager import EnsembleTimeAverager
 from domain.processes.bpm_3p_process import BPM3pProcess
 from domain.sampler.sampler import Sampler
 
-bpm3p = BPM3pProcess(0.5, 1, 1)
+bpm3p = BPM3pProcess(0.25, 1, 1)
 sampler = Sampler()
 averager = EnsembleTimeAverager()
-window_length = 100
 N = 100
-T = 1000
-time_step = 1
 min_T = 100
 max_T = 1000
+time_step = 1
 
-t = np.arange(min_T, max_T, time_step)
 ensemble = sampler.generate_ensemble(bpm3p, N, max_T, path_type='observations', time_step=time_step)
-vel_avgs = averager.average_as_function_of_t(ensemble, min_T, max_T, time_step, average_type='abs-vel')
-slope, intercept, r_value, p_value, std_err = linregress(np.log(t), np.log(vel_avgs))
+avgs_t = averager.average_as_function_of_t(ensemble, min_T, max_T, time_step, average_type='abs')
+t = np.arange(min_T, max_T, time_step)
+slope, intercept, r_value, p_value, std_err = linregress(np.log(t), np.log(avgs_t))
 
 fig, ax = plt.subplots(figsize=(8, 5))
-plt.loglog(t, vel_avgs, 'o-', label='Absolute velocity average - Slope = ' + str(slope))
+plt.loglog(t, avgs_t, 'o-', label='Absolute velocity average - Slope = ' + str(slope))
 
-ax.set_title('Absolute velocity ET-average (Normal diffusion)', fontsize=14)
-ax.set_xlabel('Time lag (Δ)', fontsize=11)
+
+ax.set_title('Absolute velocity ET-average (subdiffusion)', fontsize=14)
+ax.set_xlabel('Time (t)', fontsize=11)
 ax.xaxis.set_tick_params(labelsize=10)
 ax.xaxis.labelpad = 4
 ax.set_ylabel('Absolute velocity', fontsize=11)

scripts/averages/time_averages/bpm-3p_subdiffusion_time_absolute_velocity_average.py
@@ -18,7 +18,7 @@ vel_avgs = []
 t_axis = np.arange(100, max_T, 1)
 states_sample_path = Sampler().simulate_sample_path(bpm3p, max_T, path_type='observations', time_step=time_step, plot=False)
 for t in t_axis:
-    vel_avg = averager.average(states_sample_path, t, time_step, average_type='abs-vel')
+    vel_avg = averager.average(states_sample_path, t, time_step, average_type='abs')
     vel_avgs.append(vel_avg)
 
 slope, intercept, r_value, p_value, std_err = linregress(np.log(t_axis), np.log(vel_avgs))

src/domain/averagers/averaging_strategies/time_averaging_strategies.py
@@ -10,23 +10,23 @@ class TimeAveragingStrategy(ABC):
         pass
 
 
-class AbsoluteVelocityTimeAveragingStrategy(TimeAveragingStrategy):
+class AbsoluteTimeAveragingStrategy(TimeAveragingStrategy):
 
     def calculate(self, X, T, delta):
         n = int(T/delta)
         sum = 0
         for j in range(1, n):
-            sum += np.abs(X[j * delta] - X[(j-1) * delta])/delta
+            sum += np.abs(X[j * delta] - X[(j-1) * delta])
 
         return sum/n
 
 
-class SquareVelocityTimeAveragingStrategy(TimeAveragingStrategy):
+class SquareTimeAveragingStrategy(TimeAveragingStrategy):
 
     def calculate(self, X, T, delta):
         n = int(T/delta)
         sum = 0
         for j in range(1, n):
-            sum += ((X[j * delta] - X[(j-1) * delta])/delta) ** 2
+            sum += (X[j * delta] - X[(j-1) * delta]) ** 2
 
         return sum/n

src/domain/averagers/ensemble_time_averager.py
@@ -2,7 +2,6 @@ import numpy as np
 from scipy.stats import linregress
 
 from domain.averagers.time_averager import TimeAverager
-from domain.sampler.sampler import Sampler
 
 
 class EnsembleTimeAverager:
@@ -46,17 +45,17 @@ class EnsembleTimeAverager:
             print(f"Time-averaging trajectory n={i+1} ...")
         return np.array(avg_ensemble_t)
 
-    def estimate_moses(self, model, N, min_T, max_T, time_step=1):
+    def estimate_moses(self, ensemble, min_T, max_T, time_step=1):
         t = np.arange(min_T, max_T, time_step)
-        vel_avgs = self.average_as_function_of_t(model, N, min_T, max_T, time_step, average_type='abs-vel')
-        slope, intercept, r_value, p_value, std_err = linregress(np.log(t), np.log(vel_avgs))
+        avgs_t = self.average_as_function_of_t(ensemble, min_T, max_T, time_step, average_type='abs')
+        slope, intercept, r_value, p_value, std_err = linregress(np.log(t), np.log(avgs_t))
         M = slope + 1/2
         print(f'Moses exponent: M={M}')
         return M
 
     def estimate_noah(self, model, moses, N, min_T, max_T, time_step=1):
         t = np.arange(min_T, max_T, time_step)
-        sq_vel_avgs = self.average_as_function_of_t(model, N, min_T, max_T, time_step, average_type='sq-vel')
+        sq_vel_avgs = self.average_as_function_of_t(model, N, min_T, max_T, time_step, average_type='sq')
         slope, intercept, r_value, p_value, std_err = linregress(np.log(t), np.log(sq_vel_avgs))
         L = (slope - 2*moses + 2)/2
         print(f'Noah exponent: L={L}')