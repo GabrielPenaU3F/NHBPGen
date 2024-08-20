1df5e2887c462990da85c8a699c9502b5cc376f2
Gabriel <gpena@untref.edu.ar>
2024-08-20 02:34:21 -0300

    Added a couple more scripts

---
+++ scripts/averages/exponent_estimation/moses_and_noah_hyperballistic.py
@@ -0,0 +1,15 @@
+from domain.averagers.ensemble_time_averager import EnsembleTimeAverager
+from domain.processes.bpm_3p_process import BPM3pProcess
+from domain.sampler.sampler import Sampler
+
+bpm3p = BPM3pProcess(5/4, 1, 1)
+sampler = Sampler()
+averager = EnsembleTimeAverager()
+N = 100
+min_T = 100
+max_T = 1000
+time_step = 1
+
+ensemble = sampler.generate_ensemble(bpm3p, N, max_T, path_type='observations', time_step=time_step)
+moses = averager.estimate_moses(ensemble, min_T, max_T, time_step)
+averager.estimate_noah(ensemble, moses, min_T, max_T, time_step)
