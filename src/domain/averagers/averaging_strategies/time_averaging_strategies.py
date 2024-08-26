Modified several scripts to simplify, and refactored the time averaging strategies and time averager to fix an issue that was severely affecting performance

src/domain/averagers/averaging_strategies/time_averaging_strategies.py
@@ -6,22 +6,41 @@ import numpy as np
 
 class TimeAveragingStrategy(ABC):
 
-    def calculate(self, X: List[float], T: float, delta: int, time_step: int) -> float:
-        n = int(T / time_step)
+    def calculate(self, X: List[float], min_T: float, max_T: float, delta: int, time_step: int) -> List[float]:
+        results = [0]
+
+        T_axis = np.arange(min_T + time_step, max_T, time_step)
+        n_min = int(min_T / time_step)
         n_delta = int(delta / time_step)
+
+        if n_delta > int(min_T/time_step):
+            raise IndexError('The delta time gap must be smaller than min_T')
+
         sum_increments = 0
-        for j in range(n_delta, n):
+        for j in range(n_delta, n_min):
             n1 = j * n_delta
             n2 = (j - 1) * n_delta
             if n1 < len(X):
                 sum_increments += self.summation_term(X, n1, n2)
             else:
                 break
-        return sum_increments / n
+        results.append(sum_increments / (n_min - n_delta + 1))
+
+        for T in T_axis:
+            n = int(T / time_step)
+            if n < len(X):
+                sum_increments += self.summation_term(X, n, n - n_delta)
+                results.append(sum_increments / (n - n_delta + 1) )
+            else:
+                break
+
+        return results[1:]
+
 
     @abstractmethod
     def summation_term(self, X, n1, n2):
         pass
+
 
 
 class AbsoluteTimeAveragingStrategy(TimeAveragingStrategy):

src/domain/averagers/time_averager.py
@@ -13,13 +13,6 @@ class TimeAverager:
         'sq': SquareTimeAveragingStrategy
     }
 
-    def average(self, observations_sample_path, T, delta, time_step, average_type='regular'):
-        strategy_class = self.strategies.get(average_type)
-        if not strategy_class:
-            raise ValueError(f"Unknown average type: {average_type}")
-
-        return strategy_class().calculate(observations_sample_path, T, delta, time_step)
-
     def tamsd(self, sample_path, min_delta, max_delta, time_step=1):
         delta_axis = np.arange(min_delta, max_delta + 1)
         tamsd_delta = []
@@ -43,9 +36,8 @@ class TimeAverager:
         return np.sum(displacements) / (N - m + 1)
 
     def time_average_as_function_of_t(self, sample_path, min_T, max_T, delta, time_step, average_type):
-        avg_t = []
-        t_axis = np.arange(min_T, max_T, time_step)
-        for T in t_axis:
-            avg = self.average(sample_path, T, delta, time_step, average_type)
-            avg_t.append(avg)
-        return avg_t
+        strategy_class = self.strategies.get(average_type)
+        if not strategy_class:
+            raise ValueError(f"Unknown average type: {average_type}")
+
+        return strategy_class().calculate(sample_path, min_T, max_T, delta, time_step)