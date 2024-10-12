1b6be999e8e5b2d27e92f6358232eeb2c229a83d
Gabriel <gpena@untref.edu.ar>
2024-10-12 17:09:35 -0300

    Fixed some of the plots that were not rendering correctly

--- scripts/averages/ensemble_time_averages/bpm-3p_superdiffusion_ensemble_time_square_average.py
+++ scripts/averages/ensemble_time_averages/bpm-3p_superdiffusion_ensemble_time_square_average.py
@@ -20,31 +20,31 @@ log_avgs_t = np.log(avgs_t)
 slope, intercept, r_value, p_value, std_err = linregress(log_t, log_avgs_t)
 y_regresline = np.exp(slope * log_t + intercept)
 
-print(r_value)
-
 fig, ax = plt.subplots(figsize=(8, 5))
 ax.loglog(t, avgs_t, '-', label='Square velocity average', color='#000681', linewidth=1.8)
 ax.loglog(t[10:], y_regresline[10:], label='Regression line', color='#E71700', linewidth=1.0)
 
-ax.set_title('Square velocity ET-average (superdiffusion)', fontsize=14)
-ax.set_xlabel('Time (t)', fontsize=11)
+ax.set_title('Square velocity ET-average (superdiffusion)', fontsize=18)
+ax.set_xlabel('Time (t)', fontsize=14)
 ax.xaxis.set_tick_params(labelsize=10)
 ax.xaxis.labelpad = 4
-ax.set_ylabel('Square velocity', fontsize=11)
+ax.set_ylabel('Square velocity', fontsize=14)
 ax.yaxis.set_tick_params(labelsize=10)
 ax.yaxis.labelpad = 8
 ax.set_xlim(auto=True)
 ax.set_ylim(auto=True)
-ax.patch.set_facecolor("#ffffff")
+ax.patch.set_facecolor('white')
 ax.patch.set_edgecolor('black')
 ax.patch.set_linewidth(1)
-ax.set_facecolor("#ffffff")
-legend = ax.legend(fontsize=12)
-ax.grid(True, which='both', ls='-', linewidth=0.5)
+ax.set_facecolor('white')
+legend = ax.legend(fontsize=15, framealpha=1)
+ax.grid(True, which='both', ls='-', linewidth=0.5, alpha=1)
 
 x_pos, y_pos = 0.65, 0.75
-plt.annotate(f'Slope: {'%.3f'%(slope)}', xy=(x_pos, y_pos), xycoords='figure fraction', fontsize=11)
-plt.annotate(f'$R^2 = {'%.3f'%(r_value ** 2)}$', xy=(x_pos, y_pos - 0.05), xycoords='figure fraction', fontsize=11)
+print(slope)
+print(r_value ** 2)
+#plt.annotate(f'Slope: {'%.3f'%(slope)}', xy=(x_pos, y_pos), xycoords='figure fraction', fontsize=11)
+#plt.annotate(f'$R^2 = {'%.3f'%(r_value ** 2)}$', xy=(x_pos, y_pos - 0.05), xycoords='figure fraction', fontsize=11)
 
 fig.tight_layout()
 plt.savefig('superdif_noah.eps', format='eps')
